5073c4022614ec2f73711ec9f7f1913ab1102533
Eduardo Santamaria <98622263+edsantaM@users.noreply.github.com>
2026-05-28 23:20:59 -0600

    first task test 1
    se creó la task para la lectura y limpieza de datos junto con el envio del parquet a minio

--- ejercicio1/Airflow/dags/exercise_1/etl_engineer_challenge.py
+++ ejercicio1/Airflow/dags/exercise_1/etl_engineer_challenge.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from datetime import timedelta
 from io import BytesIO
 
+from anyio import Path
+
 import pendulum
 from airflow.decorators import dag, task
 
@@ -31,13 +33,21 @@ SOURCE_KEY = "data/data_prueba_tecnica.csv"
 def etl_engineer_challenge():
     @task
     def process_data():
-        import logging
-
+        import os
         import boto3
         import pandas as pd
-        from airflow.hooks.base import BaseHook
+        from trino.dbapi import connect
+        import logging
+        from io import BytesIO
+        import tempfile
+        logging.basicConfig(level=logging.INFO)
 
         logger = logging.getLogger(__name__)
+        #variables 
+        src_bucket = "bck-landing"
+        src_path = "data/data_prueba_tecnica.csv"
+        tgt_bucket = "bck-bronze"
+        tgt_path = "master/data_prueba_tecnica.parquet"
 
         minio_conn = BaseHook.get_connection("minio_default")
         endpoint_url = minio_conn.extra_dejson.get(
@@ -50,32 +60,129 @@ def etl_engineer_challenge():
         if not access_key or not secret_key:
             raise ValueError("The Airflow connection 'minio_default' must include login and password.")
 
-        s3 = boto3.client(
-            "s3",
-            endpoint_url=endpoint_url,
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-            region_name=minio_conn.extra_dejson.get("region_name", "us-east-1"),
-        )
+        try:
+            s3 = boto3.client(
+                "s3",
+                endpoint_url=endpoint_url,
+                aws_access_key_id=access_key,
+                aws_secret_access_key=secret_key,
+                region_name=minio_conn.extra_dejson.get("region_name", "us-east-1"),
+            )
 
-        csv_bytes = s3.get_object(Bucket=SOURCE_BUCKET, Key=SOURCE_KEY)["Body"].read()
-        df = pd.read_csv(
-            BytesIO(csv_bytes),
-            dtype="string",
-            keep_default_na=True,
-            na_values=["", "null", "NULL"],
-        )
+            csv_bytes = s3.get_object(Bucket=SOURCE_BUCKET, Key=SOURCE_KEY)["Body"].read()
+            raw = pd.read_csv(
+                BytesIO(csv_bytes),
+                dtype="string",
+                keep_default_na=True,
+                na_values=["", "null", "NULL"],
+            )
+        except Exception as e:
+            logging.info(f"Error reading CSV from S3: {e}")
+            raise
+        #hasta aca solo se lee el csv, para el analisis se creó un notebook para la ejecucion mas práctica.
 
-        logger.info("CSV loaded successfully from MinIO")
-        logger.info("DataFrame shape: rows=%s columns=%s", df.shape[0], df.shape[1])
-        logger.info("Columns: %s", df.columns.tolist())
+        #limpieza
+        try:
+            stg = raw.copy() 
+            #1. omitir id nulos
+            stg = stg[stg['id'].notna()]
 
-        return {
-            "source_bucket": SOURCE_BUCKET,
-            "source_key": SOURCE_KEY,
-            "rows_loaded": int(df.shape[0]),
-            "columns_loaded": df.columns.tolist(),
-        }
+            #2 y 3 mapear company_id a nombre y nombre a company id
+            name_to_company = (
+                raw.dropna(subset=["company_id", "name"])
+                .groupby("name", as_index=False)["company_id"]
+                .first()
+                .rename(columns={"company_id": "canonical_company_id"})
+            )
+
+            company_to_name = (
+                raw.dropna(subset=["company_id", "name"])
+                .groupby("company_id", as_index=False)["name"]
+                .first()
+                .rename(columns={"name": "canonical_name"})
+            )
+
+            stg = stg.merge(name_to_company, on ="name", how="left")
+            stg = stg.merge(company_to_name, on="company_id", how="left")
+            stg["company_id_filled"] = stg["company_id"].fillna(stg["canonical_company_id"])
+            stg["name_filled"] = stg["name"].fillna(stg["canonical_name"])
+            stg = stg.drop(columns=[ "canonical_company_id", "canonical_name"])
+
+            #4 columna de status
+            valid_statuses = {
+                "paid",
+                "voided",
+                "pending_payment",
+                "pre_authorized",
+                "refunded",
+                "charged_back",
+                "expired",
+                "partially_refunded",
+            }
+
+
+            stg["status_clean"] = stg["status"].where(stg["status"].isin(valid_statuses), "unknown")
+
+            #5. eliminar nombres sospechosos
+            stg.loc[stg["name"].isin(["MiP0xFFFF", "MiPas0xFFFF"]), "name_filled"] = "MiPasajefy"
+
+            #drop outliers en amount
+            amount_num = pd.to_numeric(stg["amount"], errors="coerce")
+            p99 = amount_num.quantile(0.99)
+
+            mask_amounts = (
+                amount_num.notna()
+                & np.isfinite(amount_num)
+                & amount_num.ge(0)
+                & amount_num.le(p99)
+            )
+
+            stg_rejected = stg[~mask_amounts].copy()
+            stg = stg[mask_amounts]
+
+
+            #result 
+            stg = stg.drop(columns=["company_id", "name", "status"])
+            stg = stg.rename(columns={"company_id_filled": "company_id", "name_filled": "name", "status_clean": "status"})
+
+            stg["is_paid"] = stg["status"].eq("paid")
+            stg["pendig_payment"] = stg["status"].eq("pending_payment")
+            stg["paid_amount"] = stg["amount"].where(stg["is_paid"], 0.0)
+            stg["pending_amount"] = stg["amount"].where(stg["pendig_payment"], 0.0)
+        except Exception as e:
+            logging.info(f"Error during data cleaning: {e}")
+            raise
+
+
+        #agregaciones 
+        try:
+            aggregated = (
+                stg.groupby(["name", "created_at"], as_index=False)
+                .agg(
+                    transactions=("id", "size"),
+                    total_amount=("amount", "sum"),
+                    average_amount=("amount", "mean"),
+                    paid_transactions=("is_paid", "sum"),
+                    paid_amount=("paid_amount", "sum"),
+                    pending_payment_transactions=("pendig_payment", "sum"),
+                    pending_amount=("pending_amount", "sum"),
+                    min_amount=("amount", "min"),
+                    max_amount=("amount", "max"),
+                )
+                .sort_values(["created_at", "name"])
+            )
+        except Exception as e:
+            logging.info(f"Error during data aggregation: {e}")
+            raise
+
+        try:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                parquet_path = Path(tmpdir) / "data_prueba_tecnica.parquet"
+                aggregated.to_parquet(parquet_path, index=False, engine="pyarrow", compression="snappy")
+                s3.upload_file(str(parquet_path), tgt_bucket, tgt_path)
+        except Exception as e:
+            logging.info(f"Error writing Parquet to S3: {e}")
+            raise
 
     process_data()
 
